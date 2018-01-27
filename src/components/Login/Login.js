b1bb847462c354f16aa9e49f183efb97ca52e7a5
rachen <anelhaman@hotmail.com>
2018-01-28 01:28:28 +0700

    login state

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import './Login.css'
 class Login extends Component {
+
+  constructor(props){
+    super(props)
+    this.state ={
+      username:'',
+      password:''
+    }
+    this.login = this.login.bind(this)
+    this.onChange = this.onChange.bind(this)
+  }
+  login(){
+      console.log("login function")
+  }
+  onChange(){
+    console.log("coming here")
+  }
+
   render() {
     return (  
       <div className="row small-up-2 medium-up-3 large-up-4">
-        <div className="column">
+        <div className="column bodyPart ">
           <h2>Login Page</h2>
+          <label>Username</label>
+          <input type="text"name="username" placeholder="username" onChange={this.onChange} />
+          <label>Password</label>
+          <input type="password" name="password" placeholder="password" onChange={this.onChange} />
+          <input type="submit" value="Login" className="button" onClick={this.login} />
+          <a href="/signup">Register</a>
         </div>
       </div>
           
